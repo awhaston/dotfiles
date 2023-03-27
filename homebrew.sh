dde00213084b56320e4099bf7357542a002e2042
Wyatt Haston <awhaston@protonmail.com>
2023-03-27 15:24:25 -0400

    Move homebrew to its own folder

---
+++ homebrew.sh
@@ -0,0 +1,25 @@
+##Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install Neovim
+brew install neovim
+
+# Install Node Version 18
+brew install node@18
+
+# Install NerdFonts
+brew tap homebrew/cask
+brew tap homebrew/cask-fonts
+brew install --cask font-roboto-mono-nerd-fontInstall Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install Neovim
+brew install neovim
+
+# Install Node Version 18
+brew install node@18
+
+# Install NerdFonts
+brew tap homebrew/cask
+brew tap homebrew/cask-fonts
+brew install --cask font-roboto-mono-nerd-font
