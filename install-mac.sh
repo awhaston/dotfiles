57c1eb4ea6701a4d7f2017c7da4ee21f7fdb4627
Wyatt Haston <awhaston@protonmail.com>
2023-03-27 15:24:25 -0400

    Move homebrew to its own folder

--- install-mac.sh
+++ install-mac.sh
@@ -1,19 +1,10 @@
-#Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# Install Neovim
-brew install neovim
-
-# Install Node Version 18
-brew install node@18
-
-# Install NerdFonts
-brew tap homebrew/cask
-brew tap homebrew/cask-fonts
-brew install --cask font-roboto-mono-nerd-font
-
 #Set Link for neovim
 ln -s ~/.dotfiles/nvim ~/.config/
 
 # Set Link for zsh
 ln -s ~/.dotfiles/.zshrc ~
+
+# Install global npm packages
+npm install -g typescript
+npm install -g nodemon
+npm install -g neovim
