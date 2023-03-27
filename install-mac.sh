1b2c9c089504d43d737e701cf8b6ccaf8b3dcbc7
Wyatt Haston <awhaston@protonmail.com>
2023-03-27 14:13:51 -0400

    renamed install script.

--- install-mac.sh
+++ install-mac.sh
@@ -4,6 +4,13 @@
 # Install Neovim
 brew install neovim
 
+# Install Node Version 18
+brew install node@18
+
+# Install NerdFonts
+brew tap homebrew/cask
+brew tap homebrew/cask-fonts
+brew install font-roboto-mono-nerd-font
 
 #Set Link for neovim
 ln -s ~/.dotfiles/nvim ~/.config/
