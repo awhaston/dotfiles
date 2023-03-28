829a48a50dcadca143c63f04bd533cd59db8fc6b
Wyatt Haston <awhaston@protonmail.com>
2023-03-27 20:32:36 -0400

    Updated install script to install packer for vim

--- install-mac.sh
+++ install-mac.sh
@@ -1,5 +1,9 @@
-#Set Link for neovim
+# Set Link for neovim
 ln -s ~/.dotfiles/nvim ~/.config/
+
+# Install Packer
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 # Set Link for zsh
 ln -s ~/.dotfiles/.zshrc ~
