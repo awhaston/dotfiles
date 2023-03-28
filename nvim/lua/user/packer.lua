ae2d6ed28b3195de845cf9f79db284ab847e034e
Wyatt Haston <awhaston@protonmail.com>
2023-03-28 18:13:48 -0400

    Added autopair, blankline and configured lsp

--- nvim/lua/user/packer.lua
+++ nvim/lua/user/packer.lua
@@ -61,6 +61,11 @@ return require('packer').startup(function(use)
         },
     }
 
+    use {
+        "windwp/nvim-autopairs",
+         config = function() require("nvim-autopairs").setup {} end
+    }
+    use "lukas-reineke/indent-blankline.nvim"
     
 end)
 
