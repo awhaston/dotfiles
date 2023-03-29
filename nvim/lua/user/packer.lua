ee9659378c897be9f1f5a78279f932a28f8fc161
Wyatt Haston <awhaston@protonmail.com>
2023-03-28 22:27:16 -0400

    Edited autopair

--- nvim/lua/user/packer.lua
+++ nvim/lua/user/packer.lua
@@ -1,46 +1,46 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-	-- Telescope
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.1',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+    -- Telescope
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-	-- Color Scheme:
-	use 'Mofiqul/vscode.nvim'
-	
-	-- Treesitter
-	use ({'nvim-treesitter/nvim-treesitter'}, {run =':TSUpdate'})
+    -- Color Scheme:
+    use 'Mofiqul/vscode.nvim'
 
-	-- Vim Fugative Git commands
-	use ('tpope/vim-fugitive')
-    
+    -- Treesitter
+    use({ 'nvim-treesitter/nvim-treesitter' }, { run = ':TSUpdate' })
+
+    -- Vim Fugative Git commands
+    use('tpope/vim-fugitive')
+
     -- LSP
     use {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v1.x',
         requires = {
             -- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {'williamboman/mason.nvim'},           -- Optional
-            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+            { 'neovim/nvim-lspconfig' },           -- Required
+            { 'williamboman/mason.nvim' },         -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
             -- Autocompletion
-            {'hrsh7th/nvim-cmp'},         -- Required
-            {'hrsh7th/cmp-nvim-lsp'},     -- Required
-            {'hrsh7th/cmp-buffer'},       -- Optional
-            {'hrsh7th/cmp-path'},         -- Optional
-            {'saadparwaiz1/cmp_luasnip'}, -- Optional
-            {'hrsh7th/cmp-nvim-lua'},     -- Optional
+            { 'hrsh7th/nvim-cmp' },       -- Required
+            { 'hrsh7th/cmp-nvim-lsp' },   -- Required
+            { 'hrsh7th/cmp-buffer' },     -- Optional
+            { 'hrsh7th/cmp-path' },       -- Optional
+            { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+            { 'hrsh7th/cmp-nvim-lua' },   -- Optional
 
             -- Snippets
-            {'L3MON4D3/LuaSnip'},             -- Required
-            {'rafamadriz/friendly-snippets'}, -- Optional
+            { 'L3MON4D3/LuaSnip' },           -- Required
+            { 'rafamadriz/friendly-snippets' }, -- Optional
         }
     }
 
@@ -52,7 +52,7 @@ return require('packer').startup(function(use)
         -- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
     }
 
-    use ('sbdchd/neoformat')
+    use('sbdchd/neoformat')
 
     use {
         'nvim-tree/nvim-tree.lua',
@@ -63,9 +63,8 @@ return require('packer').startup(function(use)
 
     use {
         "windwp/nvim-autopairs",
-         config = function() require("nvim-autopairs").setup {} end
+        config = function() require("nvim-autopairs").setup {} end
     }
-    use "lukas-reineke/indent-blankline.nvim"
-    
-end)
 
+    use "lukas-reineke/indent-blankline.nvim"
+end)
