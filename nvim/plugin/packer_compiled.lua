a3eaeedc2d52e6ba0c99a32d7b75c152c1fe44ac
Wyatt Haston <awhaston@protonmail.com>
2023-03-28 23:01:12 -0400

    Added prettier support

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -185,6 +185,11 @@ _G.packer_plugins = {
     path = "/Users/wyatthaston/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
+  ["prettier.nvim"] = {
+    loaded = true,
+    path = "/Users/wyatthaston/.local/share/nvim/site/pack/packer/start/prettier.nvim",
+    url = "https://github.com/MunifTanjim/prettier.nvim"
+  },
   ["telescope.nvim"] = {
     loaded = true,
     path = "/Users/wyatthaston/.local/share/nvim/site/pack/packer/start/telescope.nvim",
