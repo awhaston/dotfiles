157202781fb56534f663ddba03037756d712c0a4
Wyatt Haston <awhaston@protonmail.com>
2023-03-31 16:35:46 -0400

    Removed unused plugin and updated formatter with leader f

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -134,11 +134,6 @@ _G.packer_plugins = {
     path = "/Users/wyatthaston/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
-  neoformat = {
-    loaded = true,
-    path = "/Users/wyatthaston/.local/share/nvim/site/pack/packer/start/neoformat",
-    url = "https://github.com/sbdchd/neoformat"
-  },
   ["null-ls.nvim"] = {
     loaded = true,
     path = "/Users/wyatthaston/.local/share/nvim/site/pack/packer/start/null-ls.nvim",
