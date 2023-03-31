334fc2420287827624e1290ea8c6b2de00dc4644
Wyatt Haston <awhaston@protonmail.com>
2023-03-31 10:06:26 -0400

    modified nulllsp

--- nvim/after/plugin/nulllsp.lua
+++ nvim/after/plugin/nulllsp.lua
@@ -1,19 +1,20 @@
 local null_ls = require("null-ls")
 
 -- code action sources
-local code_actions = null_ls.builtins.code_actions
+-- local code_actions = null_ls.builtins.code_actions
 
 -- diagnostic sources
-local diagnostics = null_ls.builtins.diagnostics
+-- local diagnostics = null_ls.builtins.diagnostics
 
 -- formatting sources
 local formatting = null_ls.builtins.formatting
 
 -- hover sources
-local hover = null_ls.builtins.hover
+-- local hover = null_ls.builtins.hover
 
 -- completion sources
-local completion = null_ls.builtins.completion
+
+-- local completion = null_ls.builtins.completion
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
