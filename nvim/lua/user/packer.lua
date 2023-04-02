958300e2b29f5aceab48a51d20a493b28fcb2fa6
Wyatt Haston <awhaston@protonmail.com>
2023-04-01 23:32:10 -0400

    added toggleterm

--- nvim/lua/user/packer.lua
+++ nvim/lua/user/packer.lua
@@ -74,4 +74,7 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
+
+    -- Toggle Terminal
+    use { "akinsho/toggleterm.nvim", tag = '*'}
 end)
