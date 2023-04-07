0af0b3b5e9250eded901d927e593deda9ce4a74e
Wyatt Haston <awhaston@protonmail.com>
2023-04-06 22:38:15 -0400

    Added keymap files and added ctrl movement for windows

---
+++ nvim/lua/user/keys.lua
@@ -0,0 +1,11 @@
+-- Map leader key
+vim.g.mapleader = " ";
+
+-- Remap explorer (Doesn't work with nvimtree, but this can stay)
+vim.keymap.set("n", "<leader>p", vim.cmd.Ex);
+
+-- Remap Window navigation
+vim.keymap.set("n", "<C-j>", "<C-W>j");
+vim.keymap.set("n", "<C-h>", "<C-W>h");
+vim.keymap.set("n", "<C-k>", "<C-W>k");
+vim.keymap.set("n", "<C-l>", "<C-W>l");
