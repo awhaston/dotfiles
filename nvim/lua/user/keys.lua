365c865bb1a0e39d4fbe92d21e1f8c40c5d62a19
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2023-04-09 21:46:49 -0400

    Added code actions keybindings

--- nvim/lua/user/keys.lua
+++ nvim/lua/user/keys.lua
@@ -9,3 +9,5 @@ vim.keymap.set("n", "<C-j>", "<C-W>j");
 vim.keymap.set("n", "<C-h>", "<C-W>h");
 vim.keymap.set("n", "<C-k>", "<C-W>k");
 vim.keymap.set("n", "<C-l>", "<C-W>l");
+
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action);
