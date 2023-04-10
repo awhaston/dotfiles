8f59cb4b0a2a60bd2a37c3ea49c8d3ca6801ca3d
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2023-04-10 14:43:09 -0400

    Changed window management keybindings

--- nvim/lua/user/keys.lua
+++ nvim/lua/user/keys.lua
@@ -8,6 +8,8 @@ vim.g.mapleader = " ";
 vim.keymap.set("n", "<C-j>", "<C-W>j");
 vim.keymap.set("n", "<C-h>", "<C-W>h");
 vim.keymap.set("n", "<C-k>", "<C-W>k");
+vim.keymap.set("n", "<C-l>", "<C-W>l");
+vim.keymap.set("n", "<C-r>", "<C-W>r")
 
 
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action);
