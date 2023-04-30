446ae035d5cd956610ab7771747434b5768df94d
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2023-04-29 21:41:46 -0400

    removed dupe linking

--- install.py
+++ install.py
@@ -34,11 +34,6 @@ if oper == 'Darwin':
     for program in caskPrograms:
         system('brew install ' + program) 
 
-if oper != 'Windows':
-   system('ln -s ~/.dotfiles/.zshrc ~')
-   system('ln -s ~/.dotfiles/.gitconfig ~')
-   system('ln -s ~/.dotfiles/nvim ~/.config/nvim/config/')
-
 system('git config --global core.excludesfile ~/.dotfiles/.gitignore')
 
 system('git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim')
