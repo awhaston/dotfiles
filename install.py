46cdd7c128f13b50330e444c48577e0f9c2dfe2a
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2023-04-29 21:03:41 -0400

    added npm installs and linking

--- install.py
+++ install.py
@@ -4,9 +4,11 @@ import platform
 system = os.system
 oper = platform.system()
 
+npm = ['typescript', 'nodemon', 'neovim', 'prettier']
+
 if oper == 'Darwin':
     system('/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"')
-    macosPrograms = ['iterm2', 'neovim', 'node@18', 'ripgrep'] 
+    macosPrograms = ['iterm2', 'neovim', 'node@18', 'ripgrep', 'lazygit'] 
     caskPrograms = ['font-roboto-mono-nerd-font', 'linearmouse', 'tunnelblick']
 
     for program in macosPrograms:
@@ -17,3 +19,10 @@ if oper == 'Darwin':
     
     for program in caskPrograms:
         system('brew install ' + program) 
+
+if oper != 'Windows'
+   system('ln -s ~/.dotfiles/.zshrc ~')
+   system('ln -s ~/.dotfiles/.gitconfig ~')
+   system('ln -s ~/.dotfiles/nvim ~/.config/nvim/config/')
+
+system('git config --global core.excludesfile ~/.dotfiles/.gitignore')
