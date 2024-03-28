2244e6ab0d0c153f36394b27b53d4f67d857ebd2
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2024-03-28 14:41:59 -0400

    File tree now auto closes when a file is opened

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -2,6 +2,17 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
 
     opts = {
+        close_if_last_window = true,
+        event_handlers = {
+
+            {
+                event = "file_opened",
+                -- Auto close on file open
+                handler = function()
+                    require("neo-tree.command").execute({ action = "close" })
+                end,
+            },
+        },
         filesystem = {
             filtered_items = {
                 hide_by_name = {
