302c9eeb037e48653263acb6d1f48b8bb19b63ae
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2024-03-29 10:32:11 -0400

    Updated to node 20

--- homebrew.sh
+++ homebrew.sh
@@ -7,8 +7,8 @@ brew install --cask iterm2
 # Install Neovim
 brew install neovim
 
-# Install Node Version 18
-brew install node@18
+# Install Node Version 20
+brew install node@20
 
 # Install NerdFonts
 brew tap homebrew/cask
@@ -21,9 +21,6 @@ brew install neovim
 # Install ripgrep for neovim
 brew install ripgrep
 
-# Install Node Version 18
-brew install node@18
-
 # Install NerdFonts
 brew tap homebrew/cask
 brew tap homebrew/cask-fonts
@@ -31,3 +28,4 @@ brew install --cask font-roboto-mono-nerd-font
 
 # Install zsh-autocomplete
 # brew install zsh-autosuggestions
+
