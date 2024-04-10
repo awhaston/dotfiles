e6bc91000d57114346d14318d83446e0c0e59ccb
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2024-04-09 22:09:10 -0400

    Now adds tmux when configuring

--- configure-mac.sh
+++ configure-mac.sh
@@ -1,11 +1,16 @@
 # Install Packer
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+# git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # Set Link for zsh
 ln -s ~/.dotfiles/.zshrc ~
 
 # Set Link for gitconfig
 ln -s ~/.dotfiles/.gitconfig ~
+
+# Set Link for tmux
+ln -s ~/.dotfiles/tmux ~/.config/
 
 # Set Link for neovim
 ln -s ~/.dotfiles/nvim ~/.config/
