019c57b062e90dffe18e1f44f011da40c7a91026
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2024-04-16 13:08:02 -0400

    Add brew path during install

--- scripts/mac/homebrew.sh
+++ scripts/mac/homebrew.sh
@@ -1,4 +1,9 @@
 ##Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+(
+	echo
+	echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'
+) >>~/.zprofile
+
 brew bundle
