c25ff150729b5c171a7568e1a5171b60faef9df9
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2024-04-17 22:54:05 -0400

    Add keymap for :Mason and formatting

--- dot-config/nvim/lua/plugins/lsp.lua
+++ dot-config/nvim/lua/plugins/lsp.lua
@@ -1,80 +1,82 @@
 return {
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            {
-                "williamboman/mason.nvim",
-                lazy = false,
-                config = function()
-                    require("mason").setup()
-                end,
-            },
-            {
-                "hrsh7th/cmp-nvim-lsp",
-            },
-            {
-                "williamboman/mason-lspconfig.nvim",
-                lazy = false,
-                config = function()
-                    require("mason-lspconfig").setup({
-                        -- Formatters are in none-lsp.lua
-                        ensure_installed = {
-                            -- Lua
-                            "lua_ls",
-                            -- Javascript/Typescript
-                            "tsserver",
-                            "html",
-                            "tailwindcss",
-                            -- Python
-                            "pyright",
-                            -- C
-                            "cmake",
-                            "clangd",
-                        },
-                    })
-                end,
-            },
-        },
-        lazy = false,
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{
+				"williamboman/mason.nvim",
+				lazy = false,
+				config = function()
+					require("mason").setup()
 
-            -- Configuring all the servers
-            local lspconfig = require("lspconfig")
-            lspconfig.tsserver.setup({
-                capabilities = capabilities,
-            })
+					vim.keymap.set("n", "<leader>M", ":Mason <CR>", { desc = "Mason" })
+				end,
+			},
+			{
+				"hrsh7th/cmp-nvim-lsp",
+			},
+			{
+				"williamboman/mason-lspconfig.nvim",
+				lazy = false,
+				config = function()
+					require("mason-lspconfig").setup({
+						-- Formatters are in none-lsp.lua
+						ensure_installed = {
+							-- Lua
+							"lua_ls",
+							-- Javascript/Typescript
+							"tsserver",
+							"html",
+							"tailwindcss",
+							-- Python
+							"pyright",
+							-- C
+							"cmake",
+							"clangd",
+						},
+					})
+				end,
+			},
+		},
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            lspconfig.solargraph.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.html.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
+			-- Configuring all the servers
+			local lspconfig = require("lspconfig")
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
 
-            lspconfig.clangd.setup({
-                capabilities = capabilities,
-            })
+			lspconfig.solargraph.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
 
-            lspconfig.tailwindcss.setup({
-                capabilities = capabilities,
-            })
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+			})
 
-            lspconfig.pyright.setup({
-                capabilities = capabilities,
-            })
+			lspconfig.tailwindcss.setup({
+				capabilities = capabilities,
+			})
 
-            lspconfig.cmake.setup({
-                capabilities = capabilities,
-            })
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+			})
 
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover" })
-            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Goto Definition" })
-            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "Goto References" })
-            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Actions" })
-        end,
-    },
+			lspconfig.cmake.setup({
+				capabilities = capabilities,
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover" })
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Goto Definition" })
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "Goto References" })
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Actions" })
+		end,
+	},
 }
