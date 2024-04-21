5a74c7fcd47672c68c5b1262148d2d415fd0d070
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2024-04-21 18:03:31 -0400

    Add more telescope keybindings

--- dot-config/nvim/lua/plugins/telescope.lua
+++ dot-config/nvim/lua/plugins/telescope.lua
@@ -20,6 +20,16 @@ return {
             vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
             vim.keymap.set("n", "<leader><leader>", builtin.find_files, { desc = "Find Files" })
             vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Find Grep" })
+            vim.keymap.set("n", "<leader>fr", builtin.lsp_references, { desc = "Find Diagnostics" })
+            vim.keymap.set("n", "<leader>fi", builtin.lsp_implementations, { desc = "Find Implementations" })
+            vim.keymap.set("n", "<leader>ft", builtin.lsp_type_definitions, { desc = "Find Type Definitions" })
+            vim.keymap.set(
+                "n",
+                "<leader>fd",
+                ":lua require('telescope.builtin').diagnostics({bufnr = 0}) <CR>",
+                { desc = "Find Diagnostics Current Buffer" }
+            )
+            vim.keymap.set("n", "<leader>fD", builtin.diagnostics, { desc = "Find Diagnostics Project" })
         end,
     },
 }
