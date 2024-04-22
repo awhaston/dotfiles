844999ad5eba656701750213f4b9a657a77f040a
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2024-04-21 23:03:19 -0400

    Add smart yank

--- dot-config/nvim/lua/keybindings.lua
+++ dot-config/nvim/lua/keybindings.lua
@@ -9,5 +9,15 @@ map("n", "<C-k>", "<C-W>k")
 map("n", "<C-l>", "<C-W>l")
 map("n", "<C-r>", "<C-W>r")
 
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move highlighted line down" })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move highlighted line down" })
+
+vim.keymap.set("n", "<leader>sc", ":source ~/.config/nvim/init.lua <CR>", { desc = "Source configs" })
+
+vim.keymap.set("n", "dd", function()
+    if vim.api.nvim_get_current_line():match("^%s*$") then
+        return '"_dd'
+    else
+        return "dd"
+    end
+end, { expr = true, desc = "Smart Yank: Does not yank empty lines" })
