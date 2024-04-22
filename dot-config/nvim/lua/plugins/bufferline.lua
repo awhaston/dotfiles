bd8e58c86f26607efbfbcf594588d9b6506b1bcb
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2024-04-21 23:02:50 -0400

    Formatting

--- dot-config/nvim/lua/plugins/bufferline.lua
+++ dot-config/nvim/lua/plugins/bufferline.lua
@@ -1,46 +1,46 @@
 return {
-	{
-		"akinsho/bufferline.nvim",
-		version = "*",
-		dependencies = "nvim-tree/nvim-web-devicons",
-		event = "VeryLazy",
-		config = function()
-			map = vim.keymap.set
-			require("bufferline").setup({
-				options = {
+    {
+        "akinsho/bufferline.nvim",
+        version = "*",
+        dependencies = "nvim-tree/nvim-web-devicons",
+        event = "VeryLazy",
+        config = function()
+            map = vim.keymap.set
+            require("bufferline").setup({
+                options = {
                     -- stylua: ignore
                     diagnostics = "nvim_lsp",
-					always_show_bufferline = false,
-					offsets = {
-						{
-							filetype = "neo-tree",
-							text = "Neo-tree",
-							highlight = "Directory",
-							text_align = "left",
-						},
-					},
-				},
-			})
+                    always_show_bufferline = false,
+                    offsets = {
+                        {
+                            filetype = "neo-tree",
+                            text = "Neo-tree",
+                            highlight = "Directory",
+                            text_align = "left",
+                        },
+                    },
+                },
+            })
 
-			map("n", "<leader>bd", ":bd<CR>", { desc = "Close Buffer" })
-			map("n", "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", { desc = "Toggle Pin" })
-			map("n", "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", { desc = "Delete Non-Pinned Buffers" })
-			map("n", "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", { desc = "Delete Other Buffers" })
-			map("n", "<leader>br", "<Cmd>BufferLineCloseRight<CR>", { desc = "Delete Buffers to the Right" })
-			map("n", "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", { desc = "Delete Buffers to the Left" })
-			map("n", "<S-h>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev Buffer" })
-			map("n", "<S-l>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next Buffer" })
-			map("n", "[b", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev Buffer" })
-			map("n", "]b", "<cmd>BufferLineCycleNext<cr>", { desc = "Next Buffer" })
+            map("n", "<leader>bd", ":bd<CR>", { desc = "Close Buffer" })
+            map("n", "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", { desc = "Toggle Pin" })
+            map("n", "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", { desc = "Delete Non-Pinned Buffers" })
+            map("n", "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", { desc = "Delete Other Buffers" })
+            map("n", "<leader>br", "<Cmd>BufferLineCloseRight<CR>", { desc = "Delete Buffers to the Right" })
+            map("n", "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", { desc = "Delete Buffers to the Left" })
+            map("n", "<S-h>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev Buffer" })
+            map("n", "<S-l>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next Buffer" })
+            map("n", "[b", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev Buffer" })
+            map("n", "]b", "<cmd>BufferLineCycleNext<cr>", { desc = "Next Buffer" })
 
-			-- Fix bufferline when restoring a session
-			vim.api.nvim_create_autocmd("BufAdd", {
-				callback = function()
-					vim.schedule(function()
-						pcall(nvim_bufferline)
-					end)
-				end,
-			})
-		end,
-	},
+            -- Fix bufferline when restoring a session
+            vim.api.nvim_create_autocmd("BufAdd", {
+                callback = function()
+                    vim.schedule(function()
+                        pcall(nvim_bufferline)
+                    end)
+                end,
+            })
+        end,
+    },
 }
