2ff6c3955a312ca24f2d9f6a17628cbc59310383
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2024-04-27 23:22:49 -0400

    Move lazygit to tmux file and add yank to clipboard keybinding

--- dot-config/nvim/lua/keybindings.lua
+++ dot-config/nvim/lua/keybindings.lua
@@ -22,9 +22,4 @@ vim.keymap.set("n", "dd", function()
     end
 end, { expr = true, desc = "Smart Yank: Does not yank empty lines" })
 
-vim.keymap.set(
-    "n",
-    "<leader>gg",
-    "<cmd>!tmux new-window -c " .. vim.fn.getcwd() .. " -- lazygit <CR><CR>",
-    { desc = "Git Go" }
-) -- opens lazygit in a new tmux window
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank into System Clipboard" })
