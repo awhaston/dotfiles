211271c51aba774774b3ce891af1d8b6401fad35
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2024-04-27 23:21:25 -0400

    Add keybindings to open tmux

--- dot-config/nvim/lua/plugins/tmux.lua
+++ dot-config/nvim/lua/plugins/tmux.lua
@@ -1,17 +1,29 @@
 return {
-	"christoomey/vim-tmux-navigator",
-	cmd = {
-		"TmuxNavigateLeft",
-		"TmuxNavigateDown",
-		"TmuxNavigateUp",
-		"TmuxNavigateRight",
-		"TmuxNavigatePrevious",
-	},
-	keys = {
-		{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-		{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-		{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-		{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-		{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-	},
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+    cmd = {
+        "TmuxNavigateLeft",
+        "TmuxNavigateDown",
+        "TmuxNavigateUp",
+        "TmuxNavigateRight",
+        "TmuxNavigatePrevious",
+    },
+    keys = {
+        { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+        { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+        { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+        { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+        { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+
+    config = function()
+        vim.keymap.set(
+            "n",
+            "<leader>gg",
+            "<cmd>!tmux new-window -c " .. vim.fn.getcwd() .. " -- lazygit <CR><CR>",
+            { desc = "Git Go" }
+        ) -- opens lazygit in a new tmux window
+
+        vim.keymap.set("n", "<leader>th", "<cmd>!tmux split-window -l 15<CR><CR>", { desc = "Tmux Split Horizontal" })
+    end,
 }
