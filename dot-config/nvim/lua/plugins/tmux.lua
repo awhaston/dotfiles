d35079322ab81c6885075351b186ca793ae9df06
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2024-05-01 16:07:00 -0400

    Add tmux keymappings to zoom panes

--- dot-config/nvim/lua/plugins/tmux.lua
+++ dot-config/nvim/lua/plugins/tmux.lua
@@ -9,21 +9,26 @@ return {
         "TmuxNavigatePrevious",
     },
     keys = {
-        { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-        { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-        { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-        { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+        { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+        { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+        { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+        { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
         { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
     },
 
     config = function()
+        -- Enables Lazygit tmux session
         vim.keymap.set(
             "n",
             "<leader>gg",
             "<cmd>!tmux new-window -c " .. vim.fn.getcwd() .. " -- lazygit <CR><CR>",
             { desc = "Git Go" }
-        ) -- opens lazygit in a new tmux window
+        )
 
+        -- Tmux toggle horzontal pane
         vim.keymap.set("n", "<leader>th", "<cmd>!tmux split-window -l 15<CR><CR>", { desc = "Tmux Split Horizontal" })
+
+        -- Tmux zoom
+        vim.keymap.set("n", "<leader>tf", "<cmd>!tmux resize-pane -Z<CR><CR>", { desc = "Tmux Zoom Pane" })
     end,
 }
