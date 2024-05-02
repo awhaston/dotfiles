1be24c1c0a62b45cbf84d6c2aeee22ba5caabe02
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2024-05-02 10:34:19 -0400

    chore:remove unused none-ls extras

--- dot-config/nvim/lua/plugins/none-ls.lua
+++ dot-config/nvim/lua/plugins/none-ls.lua
@@ -1,56 +1,55 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
-		{
-			"jay-babu/mason-null-ls.nvim",
-			event = { "BufReadPre", "BufNewFile" },
-		},
-	},
-	config = function()
-		require("mason-null-ls").setup({
-			ensure_installed = {
-				"stylua",
-				"prettier",
-				"eslint_d",
-				"clang_format",
-				-- Go
-				"goimports",
-				"gofumpt",
-				"gomodifytags",
-				"impl",
-			},
-		})
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+        {
+            "jay-babu/mason-null-ls.nvim",
+            event = { "BufReadPre", "BufNewFile" },
+        },
+    },
+    config = function()
+        require("mason-null-ls").setup({
+            ensure_installed = {
+                "stylua",
+                "prettier",
+                "eslint_d",
+                "clang_format",
+                -- Go
+                "goimports",
+                "gofumpt",
+                "gomodifytags",
+                "impl",
+            },
+        })
 
-		local null_ls = require("null-ls")
-		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.clang_format,
-				-- Go
-				null_ls.builtins.code_actions.gomodifytags,
-				null_ls.builtins.code_actions.impl,
-				null_ls.builtins.formatting.goimports,
-				null_ls.builtins.formatting.gofumpt,
-			},
-			on_attach = function(client, bufnr)
-				if client.supports_method("textDocument/formatting") then
-					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augroup,
-						buffer = bufnr,
-						callback = function()
-							-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-							-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
-							vim.lsp.buf.format({ async = false })
-						end,
-					})
-				end
-			end,
-		})
+        local null_ls = require("null-ls")
+        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.prettier,
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.clang_format,
+                -- Go
+                null_ls.builtins.code_actions.gomodifytags,
+                null_ls.builtins.code_actions.impl,
+                null_ls.builtins.formatting.goimports,
+                null_ls.builtins.formatting.gofumpt,
+            },
+            on_attach = function(client, bufnr)
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                            -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+                            vim.lsp.buf.format({ async = false })
+                        end,
+                    })
+                end
+            end,
+        })
 
-		vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "Code Format" })
-	end,
+        vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "Code Format" })
+    end,
 }
