9b4c9bac4b0a369acfa53103bf38a6fd2d76c6b2
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2024-05-04 23:12:55 -0400

    feat: Add Golang

--- dot-config/nvim/lua/plugins/treesitter.lua
+++ dot-config/nvim/lua/plugins/treesitter.lua
@@ -1,30 +1,36 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			local config = require("nvim-treesitter.configs")
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        lazy = false,
+        config = function()
+            local config = require("nvim-treesitter.configs")
 
-			config.setup({
-				ensure_installed = {
-					"lua",
-					"javascript",
-					"bash",
-					"html",
-					"json",
-					"markdown",
-					"markdown_inline",
-					"python",
-					"query",
-					"regex",
-					"tsx",
-					"typescript",
-					"vim",
-					"yaml",
-				},
-				highlight = { enabled = true },
-				indent = { enabled = true },
-			})
-		end,
-	},
+            config.setup({
+                ensure_installed = {
+                    "lua",
+                    "javascript",
+                    "bash",
+                    "html",
+                    "json",
+                    "markdown",
+                    "markdown_inline",
+                    "python",
+                    "query",
+                    "regex",
+                    "tsx",
+                    "typescript",
+                    "vim",
+                    "yaml",
+                    "go",
+                    "gomod",
+                    "gosum",
+                    "gotmpl",
+                    "gowork",
+                },
+                highlight = { enabled = true },
+                indent = { enabled = true },
+            })
+        end,
+    },
 }
