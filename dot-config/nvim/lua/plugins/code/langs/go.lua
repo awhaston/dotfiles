4169c35730a7ee69219a657bcb52bc78559bbbd3
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2024-05-17 13:23:05 -0400

    Added nvim dap

--- dot-config/nvim/lua/plugins/code/langs/go.lua
+++ dot-config/nvim/lua/plugins/code/langs/go.lua
@@ -1,19 +1,32 @@
 return {
-    "olexsmir/gopher.nvim",
-    requires = { -- dependencies
-        "nvim-lua/plenary.nvim",
-        "nvim-treesitter/nvim-treesitter",
+    {
+
+        "olexsmir/gopher.nvim",
+        requires = { -- dependencies
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        ft = "go",
+        config = function()
+            require("gopher").setup({
+                commands = {
+                    go = "go",
+                    gomodifytags = "gomodifytags",
+                    gotests = "~/go/bin/gotests", -- also you can set custom command path
+                    impl = "impl",
+                    iferr = "iferr",
+                },
+            })
+        end,
     },
-    ft = "go",
-    config = function()
-        require("gopher").setup({
-            commands = {
-                go = "go",
-                gomodifytags = "gomodifytags",
-                gotests = "~/go/bin/gotests", -- also you can set custom command path
-                impl = "impl",
-                iferr = "iferr",
-            },
-        })
-    end,
+    {
+        "leoluz/nvim-dap-go",
+        ft = "go",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+        },
+        config = function()
+            require("dap-go").setup()
+        end,
+    },
 }
