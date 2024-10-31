173e5b2b7d04a876b748e34c4759e58547555298
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2024-10-30 22:05:59 -0500

    remove unused colorschemes

--- .config/nvim/lua/plugins/ui/colorscheme.lua
+++ .config/nvim/lua/plugins/ui/colorscheme.lua
@@ -1,25 +1,7 @@
 return {
     {
-        "navarasu/onedark.nvim",
-        name = "onedark",
-        lazy = false,
-        enabled = false,
-    },
-    {
         "kvrohit/rasmus.nvim",
         name = "rasmus",
-        lazy = false,
-        priority = 1000,
-    },
-    {
-        "pgdouyon/vim-yin-yang",
-        name = "yin",
-        lazy = false,
-        priority = 1000,
-    },
-    {
-        "pbrisbin/vim-colors-off",
-        name = "off",
         lazy = false,
         priority = 1000,
     },
