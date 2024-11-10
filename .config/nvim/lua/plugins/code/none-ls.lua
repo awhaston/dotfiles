2ac2b1b20cd84ccb9c7388a29f70c0b9739b9128
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2024-11-10 17:55:04 -0600

    Remove mason

--- .config/nvim/lua/plugins/code/none-ls.lua
+++ .config/nvim/lua/plugins/code/none-ls.lua
@@ -1,38 +1,19 @@
 return {
     "nvimtools/none-ls.nvim",
-    dependencies = {
-        {
-            "jay-babu/mason-null-ls.nvim",
-            event = { "BufReadPre", "BufNewFile" },
-        },
-    },
     config = function()
-        require("mason-null-ls").setup({
-            ensure_installed = {
-                "stylua",
-                "prettier",
-                "eslint_d",
-                "clang_format",
-                -- Go
-                "goimports",
-                "gofumpt",
-                "gomodifytags",
-                "impl",
-            },
-        })
 
         local null_ls = require("null-ls")
         local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
         null_ls.setup({
             sources = {
-                null_ls.builtins.formatting.prettier,
-                null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.clang_format,
-                -- Go
-                null_ls.builtins.code_actions.gomodifytags,
-                null_ls.builtins.code_actions.impl,
-                null_ls.builtins.formatting.goimports,
-                null_ls.builtins.formatting.gofumpt,
+                -- null_ls.builtins.formatting.prettier,
+                -- null_ls.builtins.formatting.stylua,
+                -- null_ls.builtins.formatting.clang_format,
+                -- -- Go
+                -- null_ls.builtins.code_actions.gomodifytags,
+                -- null_ls.builtins.code_actions.impl,
+                -- null_ls.builtins.formatting.goimports,
+                -- null_ls.builtins.formatting.gofumpt,
             },
             on_attach = function(client, bufnr)
                 if client.supports_method("textDocument/formatting") then
