2b8992a83580486540d8159d1ef131143339320b
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2025-01-10 12:20:25 -0600

    Updated terminal config

--- .config/nvim/lua/terminal.lua
+++ .config/nvim/lua/terminal.lua
@@ -1,9 +1,21 @@
 -- Terminal Functions
+local set = vim.opt_local
 function split_term()
     vim.cmd("belowright split")
     vim.cmd("terminal")
 end
 
+-- Set local settings for terminal buffers
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = vim.api.nvim_create_augroup("custom-term-open", {}),
+  callback = function()
+    set.number = false
+    set.relativenumber = false
+    set.scrolloff = 0
+
+    vim.bo.filetype = "terminal"
+  end,
+})
 -- Terminal keybindings
 vim.keymap.set("n", "<leader>wt", split_term, { desc = "Open Horizonal Terminal" })
 vim.keymap.set("t", "<ESC><ESC>", "<C-\\><C-N>", { desc = "Escape to normal mode" })
