191a42ea18862a6df6936fd4e0c91a65a43d8990
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2025-06-05 11:55:40 -0500

    Set word wrap to not break words

--- .config/nvim/lua/prose.lua
+++ .config/nvim/lua/prose.lua
@@ -3,13 +3,14 @@ local augroup = vim.api.nvim_create_augroup
 
 function ProseMode(args)
     vim.cmd(":set wrap!")
+    vim.cmd(":set linebreak!")
 end
 
 vim.api.nvim_create_user_command("Prose", ProseMode, { desc = "Enable prose mode" })
 
 autocmd({ "BufEnter", "BufLeave", }, {
-    pattern = { "*.md" },
+    pattern = { "*.md, *.tex" },
     group = augroup("md_files", {}),
     command = "Prose",
-    desc = "Enter Prose Mode for any md files"
+    desc = "Enter Prose Mode for any md or tex files"
 })
