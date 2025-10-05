cb85feeec73d9e671833a992bcb314213584d6c3
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2025-10-04 20:46:01 -0500

    Change lsp setup to new configuration method

--- .config/nvim/lua/plugins/code/lsp.lua
+++ .config/nvim/lua/plugins/code/lsp.lua
@@ -1,24 +1,72 @@
 return {
     {
         "awhaston/nvim-lspconfig",
-        dependencies = {
-            {
-                "awhaston/cmp-nvim-lsp", },
-        },
         lazy = false,
         config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
 
-            -- Configuring all the servers
-            -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
-            local lspconfig = require("lspconfig")
-            lspconfig.ts_ls.setup({
-                capabilities = capabilities,
+            -- Rust
+            vim.lsp.config('rust_analyzer', {
+                settings = {
+                    ['rust-analyzer'] = {
+                        diagnostics = {
+                            enable = false,
+                        }
+                    }
+                }
             })
 
-            lspconfig.phpactor.setup {}
-            lspconfig.svelte.setup {}
-            lspconfig.arduino_language_server.setup {
+            vim.lsp.enable('rust_analyzer')
+
+            vim.lsp.config('texlab', {
+                settings = {
+                    texlab = {
+                        build = {
+                            executable = "latexmk",
+                            args = {
+                                "-xelatex",
+                                "-interaction=nonstopmode",
+                                "-synctex=1",
+                                "-output-directory=build",
+                                "%f",
+                            },
+                            onSave = true,
+                            forwardSearchAfter = true,
+                            directory = "build",
+                        },
+                        auxDirectory = "build",
+                        forwardSearch = {
+                            executable = "zathura", -- or skim, okular, etc.
+                            args = { "--synctex-forward", "%l:1:%f", "%p" },
+                        },
+                    }
+                }
+            }
+            )
+
+            -- LaTeX
+            vim.lsp.enable('texlab')
+
+            -- JavaScript/TypeScript
+            vim.lsp.config('tsserver', {
+                settings = {
+                    completions = {
+                        completeFunctionCalls = true,
+                    },
+                },
+            })
+            vim.lsp.enable('tsserver')
+
+            -- PHP
+            vim.lsp.config('phpactor', {})
+            vim.lsp.enable('phpactor')
+
+            -- Svelte
+            vim.lsp.config('svelte', {})
+            vim.lsp.enable('svelte')
+
+            -- Arduino
+            vim.lsp.config('arduino_language_server', {
                 cmd = {
                     "arduino-language-server",
                     "-cli-config",
@@ -32,27 +80,18 @@ return {
                 },
                 filetypes = { "arduino" },
                 root_dir = function(fname)
-                    return lspconfig.util.root_pattern("platformio.ini")(fname) or
-                        lspconfig.util.root_pattern(".git")(fname) or
-                        lspconfig.util.path.dirname(fname)
+                    return vim.lsp.util.root_pattern("platformio.ini")(fname) or
+                        vim.lsp.util.root_pattern(".git")(fname) or
+                        vim.lsp.util.path.dirname(fname)
                 end,
-            }
-
-            vim.lsp.config('rust_analyzer', {
-                settings = {
-                    ['rust-analyzer'] = {
-                        diagnostics = {
-                            enable = false,
-                        }
-                    }
-                }
             })
+            vim.lsp.enable('arduino_language_server')
 
-            vim.lsp.enable('rust_analyzer')
-
-            lspconfig.cssls.setup({
+            -- CSS
+            vim.lsp.config('cssls', {
                 capabilities = capabilities,
             })
+            vim.lsp.enable('cssls')
 
             -- lspconfig.eslint.setup({
             --     capabilities = capabilities,
@@ -65,15 +104,15 @@ return {
             --     workingDirectories = { mode = "auto" },
             -- })
 
-            -- Ruby Lsp
-            -- lspconfig.solargraph.setup({
-            --     capabilities = capabilities,
-            -- })
 
-            lspconfig.html.setup({
+            -- HTML
+            vim.lsp.config('html', {
                 capabilities = capabilities,
             })
-            lspconfig.lua_ls.setup({
+            vim.lsp.enable('html')
+
+            -- Lua
+            vim.lsp.config('lua_ls', {
                 capabilities = capabilities,
                 settings = {
                     Lua = {
@@ -83,20 +122,30 @@ return {
                     },
                 },
             })
+            vim.lsp.enable('lua_ls')
 
-            lspconfig.clangd.setup({
+            -- C/C++
+            vim.lsp.config('clangd', {
                 capabilities = capabilities,
             })
+            vim.lsp.enable('clangd')
 
-            -- lspconfig.tailwindcss.setup({
+
+            -- Tailwind CSS
+            -- vim.lsp.config('tailwindcss', {
             --     capabilities = capabilities,
             -- })
+            -- vim.lsp.enable('tailwindcss')
 
-            lspconfig.pyright.setup({
+            -- Python
+            vim.lsp.config('pyright', {
                 capabilities = capabilities,
             })
+            vim.lsp.enable('pyright')
 
-            lspconfig.gopls.setup({})
+            -- Go
+            vim.lsp.config('gopls', {})
+            vim.lsp.enable('gopls')
 
             -- LSP Keybindings
             vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover" })
